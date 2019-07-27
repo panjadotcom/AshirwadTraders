59eea5c43f181d314f88a77672a8fcd321c06f4c
Pankaj Yadav <panjadotcom@hotmail.com>
2019-07-27 19:27:34 +0530

    First Commit

---
+++ AshirwadTraders/LoginPage.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace AshirwadTraders
+{
+    partial class LoginPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainerLogin = new System.Windows.Forms.SplitContainer();
+            this.pictureBoxCompanyLogo = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanelButtons = new System.Windows.Forms.TableLayoutPanel();
+            this.buttonSetting = new System.Windows.Forms.Button();
+            this.buttonChangePasswd = new System.Windows.Forms.Button();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.labelUsername = new System.Windows.Forms.Label();
+            this.labelPassword = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerLogin)).BeginInit();
+            this.splitContainerLogin.Panel1.SuspendLayout();
+            this.splitContainerLogin.Panel2.SuspendLayout();
+            this.splitContainerLogin.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCompanyLogo)).BeginInit();
+            this.tableLayoutPanelButtons.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainerLogin
+            // 
+            this.splitContainerLogin.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainerLogin.IsSplitterFixed = true;
+            this.splitContainerLogin.Location = new System.Drawing.Point(0, 0);
+            this.splitContainerLogin.Name = "splitContainerLogin";
+            // 
+            // splitContainerLogin.Panel1
+            // 
+            this.splitContainerLogin.Panel1.Controls.Add(this.pictureBoxCompanyLogo);
+            // 
+            // splitContainerLogin.Panel2
+            // 
+            this.splitContainerLogin.Panel2.Controls.Add(this.tableLayoutPanelButtons);
+            this.splitContainerLogin.Size = new System.Drawing.Size(689, 319);
+            this.splitContainerLogin.SplitterDistance = 360;
+            this.splitContainerLogin.SplitterWidth = 1;
+            this.splitContainerLogin.TabIndex = 0;
+            // 
+            // pictureBoxCompanyLogo
+            // 
+            this.pictureBoxCompanyLogo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxCompanyLogo.Image = global::AshirwadTraders.Properties.Resources.Setting;
+            this.pictureBoxCompanyLogo.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxCompanyLogo.Name = "pictureBoxCompanyLogo";
+            this.pictureBoxCompanyLogo.Size = new System.Drawing.Size(360, 319);
+            this.pictureBoxCompanyLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxCompanyLogo.TabIndex = 0;
+            this.pictureBoxCompanyLogo.TabStop = false;
+            // 
+            // tableLayoutPanelButtons
+            // 
+            this.tableLayoutPanelButtons.ColumnCount = 10;
+            this.tableLayoutPanelButtons.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanelButtons.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanelButtons.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanelButtons.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanelButtons.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanelButtons.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanelButtons.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanelButtons.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanelButtons.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanelButtons.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanelButtons.Controls.Add(this.buttonSetting, 7, 0);
+            this.tableLayoutPanelButtons.Controls.Add(this.buttonChangePasswd, 0, 10);
+            this.tableLayoutPanelButtons.Controls.Add(this.buttonLogin, 5, 10);
+            this.tableLayoutPanelButtons.Controls.Add(this.textBoxPassword, 0, 8);
+            this.tableLayoutPanelButtons.Controls.Add(this.textBoxUsername, 0, 5);
+            this.tableLayoutPanelButtons.Controls.Add(this.labelUsername, 0, 4);
+            this.tableLayoutPanelButtons.Controls.Add(this.labelPassword, 0, 7);
+            this.tableLayoutPanelButtons.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanelButtons.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tableLayoutPanelButtons.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanelButtons.Name = "tableLayoutPanelButtons";
+            this.tableLayoutPanelButtons.RowCount = 15;
+            this.tableLayoutPanelButtons.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.tableLayoutPanelButtons.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.tableLayoutPanelButtons.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.tableLayoutPanelButtons.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.tableLayoutPanelButtons.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.tableLayoutPanelButtons.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.tableLayoutPanelButtons.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.tableLayoutPanelButtons.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.tableLayoutPanelButtons.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.tableLayoutPanelButtons.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.tableLayoutPanelButtons.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.tableLayoutPanelButtons.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.tableLayoutPanelButtons.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.tableLayoutPanelButtons.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.tableLayoutPanelButtons.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+            this.tableLayoutPanelButtons.Size = new System.Drawing.Size(328, 319);
+            this.tableLayoutPanelButtons.TabIndex = 0;
+            // 
+            // buttonSetting
+            // 
+            this.buttonSetting.BackColor = System.Drawing.SystemColors.Control;
+            this.buttonSetting.BackgroundImage = global::AshirwadTraders.Properties.Resources.Setting;
+            this.buttonSetting.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.tableLayoutPanelButtons.SetColumnSpan(this.buttonSetting, 3);
+            this.buttonSetting.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonSetting.Location = new System.Drawing.Point(227, 3);
+            this.buttonSetting.Name = "buttonSetting";
+            this.tableLayoutPanelButtons.SetRowSpan(this.buttonSetting, 3);
+            this.buttonSetting.Size = new System.Drawing.Size(98, 57);
+            this.buttonSetting.TabIndex = 0;
+            this.buttonSetting.UseVisualStyleBackColor = false;
+            // 
+            // buttonChangePasswd
+            // 
+            this.tableLayoutPanelButtons.SetColumnSpan(this.buttonChangePasswd, 5);
+            this.buttonChangePasswd.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonChangePasswd.Location = new System.Drawing.Point(3, 213);
+            this.buttonChangePasswd.Name = "buttonChangePasswd";
+            this.tableLayoutPanelButtons.SetRowSpan(this.buttonChangePasswd, 2);
+            this.buttonChangePasswd.Size = new System.Drawing.Size(154, 36);
+            this.buttonChangePasswd.TabIndex = 1;
+            this.buttonChangePasswd.Text = "CHANGE PASSWORD";
+            this.buttonChangePasswd.UseVisualStyleBackColor = true;
+            // 
+            // buttonLogin
+            // 
+            this.tableLayoutPanelButtons.SetColumnSpan(this.buttonLogin, 5);
+            this.buttonLogin.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonLogin.Location = new System.Drawing.Point(163, 213);
+            this.buttonLogin.Name = "buttonLogin";
+            this.tableLayoutPanelButtons.SetRowSpan(this.buttonLogin, 2);
+            this.buttonLogin.Size = new System.Drawing.Size(162, 36);
+            this.buttonLogin.TabIndex = 2;
+            this.buttonLogin.Text = "LOGIN";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            // 
+            // textBoxPassword
+            // 
+            this.tableLayoutPanelButtons.SetColumnSpan(this.textBoxPassword, 10);
+            this.textBoxPassword.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxPassword.Location = new System.Drawing.Point(3, 171);
+            this.textBoxPassword.MaxLength = 25;
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.PasswordChar = '*';
+            this.textBoxPassword.Size = new System.Drawing.Size(322, 21);
+            this.textBoxPassword.TabIndex = 3;
+            // 
+            // textBoxUsername
+            // 
+            this.tableLayoutPanelButtons.SetColumnSpan(this.textBoxUsername, 10);
+            this.textBoxUsername.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxUsername.Location = new System.Drawing.Point(3, 108);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(322, 21);
+            this.textBoxUsername.TabIndex = 4;
+            // 
+            // labelUsername
+            // 
+            this.labelUsername.AutoSize = true;
+            this.tableLayoutPanelButtons.SetColumnSpan(this.labelUsername, 10);
+            this.labelUsername.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelUsername.Location = new System.Drawing.Point(3, 84);
+            this.labelUsername.Name = "labelUsername";
+            this.labelUsername.Size = new System.Drawing.Size(322, 21);
+            this.labelUsername.TabIndex = 5;
+            this.labelUsername.Text = "LOGIN";
+            this.labelUsername.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.tableLayoutPanelButtons.SetColumnSpan(this.labelPassword, 10);
+            this.labelPassword.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelPassword.Location = new System.Drawing.Point(3, 147);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(322, 21);
+            this.labelPassword.TabIndex = 6;
+            this.labelPassword.Text = "PASSWORD";
+            this.labelPassword.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // LoginPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(689, 319);
+            this.Controls.Add(this.splitContainerLogin);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "LoginPage";
+            this.Text = "Login";
+            this.splitContainerLogin.Panel1.ResumeLayout(false);
+            this.splitContainerLogin.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerLogin)).EndInit();
+            this.splitContainerLogin.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCompanyLogo)).EndInit();
+            this.tableLayoutPanelButtons.ResumeLayout(false);
+            this.tableLayoutPanelButtons.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainerLogin;
+        private System.Windows.Forms.PictureBox pictureBoxCompanyLogo;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanelButtons;
+        private System.Windows.Forms.Button buttonSetting;
+        private System.Windows.Forms.Button buttonChangePasswd;
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.Label labelUsername;
+        private System.Windows.Forms.Label labelPassword;
+    }
+}
+
--- AshirwadTraders/LoginPage.cs
+++ AshirwadTraders/LoginPage.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace AshirwadTraders
 {
-    public partial class Form1 : Form
+    public partial class LoginPage : Form
     {
-        public Form1()
+        public LoginPage()
         {
             InitializeComponent();
         }
